4396a981f6356c1f1eae91d3b17d72a8934d605a
Troxx <121587043+xx0rT@users.noreply.github.com>
2025-12-19 21:13:31 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  optimizeDeps: {
+    include: ['next-themes'],
+  },
   server: {
     port: 3000,
     open: true,
