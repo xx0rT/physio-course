2495061a01d992d0654083e825a99d0dfa57da24
Troxx <121587043+xx0rT@users.noreply.github.com>
2025-12-20 09:08:09 +0100

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     },
   },
   optimizeDeps: {
-    include: ['next-themes'],
+    include: ['next-themes', '@supabase/supabase-js'],
   },
   server: {
     port: 3000,
